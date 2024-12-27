591d31d9da44a8e31a9788c9942b7ea1382d07ad
dfujah <dfujah@umich.edu>
2024-12-27 09:30:07 -0500

    separated business logic from the routes, server starts in index.js file

---
+++ journaling-backend/src/api/home.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser') //make sure this is installed properly
+const Database = require('../utils/dynamoDB')
+const userController = require('../controller/homeController') 
+
+const router = express.Router()
+const app = express()
+
+// middleware 
+router.use(bodyParser.json())
+
+
+// home page routes
+router.get('/', userController.displayDaily);
+router.post('/', userController.newUser);
+router.get('/login', userController.loginUser);
+
+
+
+module.exports = router;
