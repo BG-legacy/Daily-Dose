85716ea0f4cd9154e642cf70bd5829b6e90b2d5c
Jan <55969944+janrebolledo@users.noreply.github.com>
2024-12-29 23:15:51 -0800

    Initialized Next Auth, Added Folder Structure + Utils

---
+++ frontend/app/utils/user.js
@@ -0,0 +1,31 @@
+const db = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.DB_URL
+
+/** 
+ * @typedef user
+ * @type {object}
+ * @property {string} userID
+ * @property {string} name
+ * @property {string} email
+
+/**
+ * Create a new user
+ * @param {user} user
+ */
+export async function newUser({ userID, name, email }) {
+  const fetch = await fetch(db, { method: 'POST', body: JSON.stringify({ userID, name, email }) })
+  const res = await fetch.json()
+
+  return res
+}
+
+/** 
+ * Login user
+ * @param {string} userID
+ * @returns {object} user
+ */
+export async function loginUser(userID) {
+  const res = await fetch(`${db}/login`, { method: 'POST', body: JSON.stringify({ userID: userID }) })
+  const user = res.json()
+
+  return user
+}
