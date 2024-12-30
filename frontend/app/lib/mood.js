d5627e87befb0f5bc5909add886a939a73e2451b
Jan <55969944+janrebolledo@users.noreply.github.com>
2024-12-29 23:15:51 -0800

    Initialized Next Auth, Added Folder Structure + Utils

---
+++ frontend/app/lib/mood.js
@@ -0,0 +1,15 @@
+/** @todo create helper functions to set mood
+ */
+
+
+/**
+ * Set user's mood for today
+ * @param {('happy' | 'sad' | 'upset')} mood - User inputted mood
+ * @param {string} userId - User ID
+ */
+
+export async function setMood(mood, userId) {
+
+
+  return
+}
