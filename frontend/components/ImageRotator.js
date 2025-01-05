0374f26995d8b8cd6dccfd5ead97ab908208ff1c
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2025-01-04 23:58:59 -0500

    Updated Landing Page Images

--- frontend/components/ImageRotator.js
+++ frontend/components/ImageRotator.js
@@ -4,15 +4,19 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import heroImage from '../public/assets/media/snow.jpg';
 import sunnyImage from '../public/assets/media/betterdays.png';
+import restlessNights from '../public/assets/media/restlessnights.png';
+import uncertainTimes from '../public/assets/media/uncertaintimes.png';
 import happy from '../public/assets/brand/Happy.png';
 import sad from '../public/assets/brand/Sad.png';
+import upset from '../public/assets/brand/Upset.png';
+import uncertain from '../public/assets/brand/Uncertain.png';
 
 export default function ImageRotator({ setCurrentText, setCurrentEmoticon }) {
   const [currentImage, setCurrentImage] = useState(heroImage);
   const [fade, setFade] = useState(false);
 
   useEffect(() => {
-    const images = [heroImage, sunnyImage];
+    const images = [heroImage, sunnyImage, restlessNights, uncertainTimes];
     let index = 0;
 
     const textElement = document.getElementById('hero-text')
@@ -29,14 +33,20 @@ export default function ImageRotator({ setCurrentText, setCurrentEmoticon }) {
         if (images[index] === heroImage) {
           setCurrentText("Gloomy Days");
           setCurrentEmoticon(sad);
-        } else {
+        } else if (images[index] === sunnyImage) {
           setCurrentText("Better Days");
           setCurrentEmoticon(happy);
+        } else if (images[index] === restlessNights) {
+          setCurrentText("Restless Nights");
+          setCurrentEmoticon(upset);
+        } else {
+          setCurrentText("Uncertain Times");
+          setCurrentEmoticon(uncertain);
         }
         setFade(false);
       }, 1000); //the fade duration (1sec)
 
-    }, 7000); //the duration for each image (7sec)
+    }, 4000); //the duration for each image (4sec)
     return () => clearInterval(interval);
   }, [setCurrentText, setCurrentEmoticon]);
 
