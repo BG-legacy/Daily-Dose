9a4675dcdee3603b852850f8b6fca18767ad3934
dfujah <dfujah@umich.edu>
2025-01-09 21:45:55 -0500

    testing logged in users are able to access both journals and mood tables

--- frontend/tests/testAuth.js
+++ frontend/tests/testAuth.js
@@ -1,20 +1,20 @@
 // import useAuth from '../auth.js';
 // // this should simulate a react env
 
-// const {userID, signIn, signOut, logout} = useAuth();
+const {userID, signIn, signOut, logout} = useAuth();
 
 // Test sign up with example credentials
-// signUp({
-//     "userID": "icky9",
-//     "name" : "rky9 cot",
-//     "email": "ickycot@test89.com",
-//     "password": "7844766556"
-// });
+signUp({
+    "userID": "icky9",
+    "name" : "rky9 cot",
+    "email": "ickycot@test89.com",
+    "password": "7844766556"
+});
 
-// signIn({
-//     email: 'rickycot@test89.com',
-//     password: '7844766556'
-// })
+signIn({
+    email: 'rickycot@test89.com',
+    password: '7844766556'
+})
 
 //TODO: either separate the logic and make it outside of react
 
