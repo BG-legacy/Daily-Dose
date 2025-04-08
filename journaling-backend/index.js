2fd32da4cc3b320fef7a8427eaf554fdbd602fda
BG-legacy <bginnjr20@gmail.com>
2025-04-08 18:41:14 -0400

    fixed streak issues

--- journaling-backend/index.js
+++ journaling-backend/index.js
@@ -39,7 +39,7 @@ app.use(cors({
     : 'https://www.daily-dose.me', // Frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'cache-control']
 }));
 
 // Body parsing middleware
